d30f31d685273c680a621a8902bde83c5d3ca73f
ronSiuuuu <mehedihasanakash1717@gmail.com>
2025-09-25 01:23:06 +0600

    polished whyUs page

--- src/app/components/WhyChoose.jsx
+++ src/app/components/WhyChoose.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { IoIosArrowDroprightCircle } from 'react-icons/io';
+import Container from './Container';
 
 const WhyChoose = () => {
     return (
-        <div className='w-11/12 mx-auto my-20'>
-            <div className='mb-20 ml-44'>
-                <h1 className='font-bold text-4xl'>Why Choose Our AI Job <br /> Placement Platform?</h1>
+        <Container className=' my-16'>
+            <div className='mb-16 '>
+                <h1 className='font-bold lg:text-4xl text-2xl'>Why Choose Our AI Job <br /> Placement Platform?</h1>
                 <img src="/images/line.png" alt="" />
             </div>
-            <div className='flex gap-6 justify-center items-center'>
-                <div>
+            <div className='flex flex-col lg:flex-row gap-10 lg:gap-20   items-center'>
+                <div className='space-y-4'>
                     <h1 className='font-semibold text-4xl'>Smart Resume Tailoring</h1>
                     <p>Let our smart AI craft the perfect resume for every <br /> job—packed with the right keywords, skills,
                      and <br /> experiences to grab recruiters’ attention and land <br /> interviews faster.</p>
@@ -18,10 +19,10 @@ const WhyChoose = () => {
                {/* right side graphic */}
         <div className="relative md:w-1/2">
           {/* floating labels */}
-          <span className="absolute -top-4 left-1/3 -translate-x-1/2 bg-white shadow-md px-3 py-1 rounded-md font-medium text-sm">
+          <span className="absolute hidden lg:block -top-4 left-1/3 -translate-x-1/2 bg-white shadow-md px-3 py-1 rounded-md font-medium text-sm">
             Job Description
           </span>
-          <span className="absolute top-1/6   bg-white shadow-md px-3 py-1 rounded-md font-medium text-sm">
+          <span className="absolute hidden lg:block top-1/6   bg-white shadow-md px-3 py-1 rounded-md font-medium text-sm">
             Resume
           </span>
 
@@ -33,7 +34,7 @@ const WhyChoose = () => {
           />
 
           {/* feature list */}
-          <ul className="absolute top-1/12 right-0 space-y-2 text-gray-700 text-sm">
+          <ul className="absolute top-2/12 z-10 right-0 hidden lg:block space-y-2 text-gray-700 text-sm">
             <li className="flex items-center gap-2">
                Customize Resume<span className="text-blue-500">•</span>
             </li>
@@ -50,7 +51,7 @@ const WhyChoose = () => {
         </div>
             </div>
 
-        </div>
+        </Container>
     );
 };
 
