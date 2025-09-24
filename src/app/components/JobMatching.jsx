675f4a4c5416f675c4eae54eed5e5d079997084f
ronSiuuuu <mehedihasanakash1717@gmail.com>
2025-09-25 01:25:59 +0600

    polished jobMatching section

--- src/app/components/JobMatching.jsx
+++ src/app/components/JobMatching.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import Container from './Container';
 
 const JobMatching = () => {
     return (
-        <div className='w-11/12 mx-auto'>
-            <div className='flex items-center gap-16'>
-                <img className='ml-24' src="/images/jobMatch.png" alt="" />
+        <Container >
+            <div className='flex flex-col lg:flex-row items-center gap-10 lg:gap-20'>
+                <img  src="/images/jobMatch.png" alt="" />
                 <div className='space-y-4'>
                     <h1 className='font-bold text-4xl'>Lightning-Fast Job <br /> Matching</h1>
                     <p>AI-powered 1:1 Job to Resume matching that positions you <br /> perfectly aligned in the competitive Job market with your skills, <br /> salary expectations, and career goals.</p>
                     <img className='opacity-60' src="/images/line.png" alt="" />
                 </div>
             </div>
-        </div>
+        </Container>
     );
 };
 
